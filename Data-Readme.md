d25dbffb8e448bde12f0bc6492a6e71ea20268d6
Elaphurus <43885619+Lennox-Elaphurus@users.noreply.github.com>
2020-12-19 00:28:23 +0800

    Finish predict_data vector

--- Data-Readme.md
+++ Data-Readme.md
@@ -1,8 +1,14 @@
 # Data-Readme
 
-## weibo_train_data-docVector.txt
+- 训练集：
+	- 数据特征：weibo_train_data-docVector.txt
+	- 标签：
 
-训练集的文本向量化，每篇博文转为一个100维32位浮点数表示的向量。
+-	测试集：
+	-	weibo_predict_data-docVector.txt
+
+
+训练集的文本向量化，每篇博文转为一个100维32位浮点数表示的向量，没有列标签，数据按照原始数据中的顺序排列。
 
 可直接用`dataV=np.loadtxt(fname="midData/weibo_train_data-docVector.txt",dtype=float)`读取向量
 
--- preprocess.py
+++ preprocess.py
@@ -36,6 +36,14 @@ def importDoc(filename):
     print(doc)
     pickleDump(doc,filename+"-doc-list")
 
+
+def importDoc2(filename):
+    doc=np.loadtxt(fname="rawData/"+filename+".txt",delimiter='\t',comments='\n', usecols=(3),dtype=str,encoding='utf-8')
+    doc=doc.tolist()
+    print(doc)
+    pickleDump(doc,filename+"-doc-list")
+
+
 def cut_sentence(data,filename):
     stopList=[line[:-1] for line in open("中文停用词表.txt",'r',encoding='utf-8')]
     for i in range(len(data)):
@@ -48,13 +56,34 @@ def cut_sentence(data,filename):
     pickleDump(data,filename+"-doc-cut")
 
 
+def cut_sentence2(data,filename):
+    stopList=[line[:-1] for line in open("中文停用词表.txt",'r',encoding='utf-8')]
+    for i in range(len(data)):
+        doc=jieba.cut(data[i])
+        doc=" ".join(doc).split()
+        doc=[word for word in doc if word not in stopList]
+        # print(doc)
+        data[i]=doc
+    print(data)
+    pickleDump(data,filename+"-doc-cut")
+
+
 def getTrainSet(data,filename):
     data=np.array(data,dtype=object)
     TD=[]
     for i in range(data.shape[0]):
         TD.append(TaggedDocument(data[i][1],tags=[i]))
-    # print(TD)
+    print(TD)
     pickleDump(TD,filename+"-trainSet")
+
+
+def getTrainSet2(data,filename):
+    # data=np.array(data,dtype=object)
+    TD=[]
+    for i in range(len(data)):
+        TD.append(TaggedDocument(data[i],tags=[i]))
+    print(TD)
+    pickleDump(TD,filename+"-testSet")
 
 def Doc2VecTrain(data,filename):
     # print(data)
@@ -76,6 +105,15 @@ def inferFromModel(trainSet,filename):
     np.savetxt(os.getcwd()+"/midData/"+filename+"-docVector.txt",dataVec)
 
 
+def inferFromModel2(testSet,filename):
+    fname = get_tmpfile(os.getcwd()+"/midData/weibo_train_data-doc2vec.model")
+    model = Doc2Vec.load(fname)
+    dataVec = [model.infer_vector(z.words) for z in testSet]
+    # dataVec=model.infer_vector(testSet[0].words)
+    print("samples =",len(dataVec),", dimemsion =",dataVec[0].shape[0])
+    # print(dataVec)
+    np.savetxt(os.getcwd()+"/midData/"+filename+"-docVector.txt",dataVec)
+
 
 #############
 # main weibo_train_data
@@ -84,17 +122,23 @@ def inferFromModel(trainSet,filename):
 # importTag("weibo_train_data")
 # importDoc("weibo_train_data")
 
+# importDoc2("weibo_predict_data")
+
 # data=pickleLoad("weibo_train_data-doc-list")
 # cut_sentence(data,"weibo_train_data")
+# data=pickleLoad("weibo_predict_data-doc-list")
+# cut_sentence2(data,"weibo_predict_data")
 
 # dataL=pickleLoad("weibo_train_data-doc-cut")
 # getTrainSet(dataL,"weibo_train_data")
-#
+# dataL=pickleLoad("weibo_predict_data-doc-cut")
+# getTrainSet2(dataL,"weibo_predict_data")
+
 # trainSet=pickleLoad("weibo_train_data-trainSet")
 # Doc2VecTrain(trainSet,"weibo_train_data")
 
+
 # inferFromModel(trainSet ,"weibo_train_data")
+testSet=pickleLoad("weibo_predict_data-testSet")
+inferFromModel2(testSet ,"weibo_predict_data")
 
-
-testV=np.loadtxt(fname="midData/test-docVector.txt",dtype=float)
-print(testV)
