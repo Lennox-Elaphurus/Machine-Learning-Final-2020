660f3416268827d7878730f1f3988a6d730dd09f
Elaphurus <43885619+Lennox-Elaphurus@users.noreply.github.com>
2020-12-18 21:27:21 +0800

    Finish getTrainSet

--- preprocess.py
+++ preprocess.py
@@ -4,7 +4,7 @@ import numpy as np
 import gensim
 import jieba
 from gensim.models.doc2vec import Doc2Vec
-
+from gensim.models.doc2vec import TaggedDocument
 
 # load from pickle file
 def pickleLoad(filename):
@@ -24,33 +24,52 @@ def pickleDump(variable,filename):
 
 
 def importTag(filename):
-    tag=np.loadtxt(fname="rawData/"+filename+".txt",dtype=str,delimiter='\t',comments='\n', usecols=(3,4,5),encoding='utf-8')
+    tag=np.loadtxt(fname="rawData/"+filename+".txt",dtype=str,delimiter='\t',comments='\n', usecols=(1,3,4,5),encoding='utf-8')
     print(tag)
-    pickleDump(tag,filename+"-tag-noMid")
+    pickleDump(tag,filename+"-tag")
 
 
 def importDoc(filename):
-    doc=np.loadtxt(fname="rawData/"+filename+".txt",delimiter='\t',comments='\n', usecols=(6),dtype=str,encoding='utf-8')
+    doc=np.loadtxt(fname="rawData/"+filename+".txt",delimiter='\t',comments='\n', usecols=(1,6),dtype=str,encoding='utf-8')
+    doc=doc.tolist()
     print(doc)
-    pickleDump(doc,filename+"-doc-noMid")
+    pickleDump(doc,filename+"-doc-list")
 
-def cut_sentence(data):
+def cut_sentence(data,filename):
     stopList=[line[:-1] for line in open("中文停用词表.txt",'r',encoding='utf-8')]
-    result=[]
-    for i in range(data.shape[0]):
+    for i in range(len(data)):
         doc=jieba.cut(data[i][1])
         doc=" ".join(doc).split()
         doc=[word for word in doc if word not in stopList]
-        print(doc)
-        data[i][1]=list(doc)
+        # print(doc)
+        data[i][1]=doc
+    print(data)
+    pickleDump(data,filename+"-doc-cut")
 
+
+def getTrainSet(data,filename):
+    data=np.array(data,dtype=object)
+    TD=TaggedDocument(data[:,1],tags=data[:,0])
+    print(TD)
+    pickleDump(TD,filename+"-trainSet")
+
+
+def Doc2VecTrain(data,filename):
+    model=Doc2Vec(documents=data)
+    model.train(data)
 
 #############
 # main weibo_train_data
-
+# importTag("test")
+# importDoc("test")
 # importTag("weibo_train_data")
 # importDoc("weibo_train_data")
 
-# data=pickleLoad("test-doc")
-# cut_sentence(data)
-# print(data)
+# data=pickleLoad("weibo_train_data-doc-list")
+# cut_sentence(data,"weibo_train_data")
+
+# dataL=pickleLoad("weibo_train_data-doc-cut")
+# getTrainSet(dataL,"weibo_train_data")
+
+trainSet=pickleLoad("test-trainSet")
+Doc2VecTrain(trainSet,"test")
